6750e93d1a13e07871364804ce85b18e0465d6fb
mahdi <mahpython@gmail.com>
2022-08-20 17:33:36 +0430

    fix a bug in bestoon/view

--- bestoon/views.py
+++ bestoon/views.py
@@ -13,13 +13,15 @@ class HomePage(ListView):
 	def get_context_data(self):
 		context = super().get_context_data()
 		user = self.request.user
-		context['incomesum'] = Income.objects.filter(user=user).aggregate(Sum('amount'))
-		context['incomecount'] = Income.objects.filter(user=user).aggregate(Count('title'))
-		context['expensesum'] = Expense.objects.filter(user=user).aggregate(Sum('amount'))
-		context['expensecount'] = Expense.objects.filter(user=user).aggregate(Count('title'))
-		context['difference'] = Income.objects.filter(user=user).aggregate(
-			Sum('amount'))['amount__sum'] - Expense.objects.filter(user=user).aggregate(
-			Sum('amount'))['amount__sum']
+		if Income.objects.filter(user=user) and Expense.objects.filter(user=user):
+
+			context['incomesum'] = Income.objects.filter(user=user).aggregate(Sum('amount'))
+			context['incomecount'] = Income.objects.filter(user=user).aggregate(Count('title'))
+			context['expensesum'] = Expense.objects.filter(user=user).aggregate(Sum('amount'))
+			context['expensecount'] = Expense.objects.filter(user=user).aggregate(Count('title'))
+			context['difference'] = Income.objects.filter(user=user).aggregate(
+				Sum('amount'))['amount__sum'] - Expense.objects.filter(user=user).aggregate(
+				Sum('amount'))['amount__sum']
 		return context
 
 
